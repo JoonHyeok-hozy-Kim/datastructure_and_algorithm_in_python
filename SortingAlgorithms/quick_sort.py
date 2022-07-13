12077be672402bd69b7dc5307feb6d6b66aa1155
hozy_galaxy_book <danielisdan@naver.com>
2022-07-13 14:48:50 +0900

    randomized in_place_quick sort

--- SortingAlgorithms/quick_sort.py
+++ SortingAlgorithms/quick_sort.py
@@ -29,23 +29,46 @@ def quick_sort(S):
         S.enqueue(G.dequeue())
 
 
+from random import randint
 def inplace_quick_sort(S, a=0, b=None):
     if b is None:
         b = len(S)-1
     if a >= b: return
+
+    # Random pivot selection
+    p = randint(a, b)
+    S[p], S[b] = S[b], S[p]
+
     pivot = S[b]
     left = a
     right = b-1
+    # print('[Initial] pivot : {}, {}'.format(pivot, S))
     while left <= right:
+        # print(' -> left : {}'.format(S[left]), end='')
         while left <= right and S[left] < pivot:
             left += 1
+        #     print(' -> {}'.format(S[left]), end='')
+        # print('\n -> right : {}'.format(S[right]), end='')
         while left <= right and pivot < S[right]:
             right -= 1
+            # print(' -> {}'.format(S[right]), end='')
 
         if left <= right:
             S[left], S[right] = S[right], S[left]
             left, right = left + 1, right - 1
+            # print('\n[After swap 1] {}'.format(S))
+            # print(' left : {}, right : {}'.format(S[left], S[right]))
 
     S[left], S[b] = S[b], S[left]
+    # print('\n[After swap 2] {}'.format(S))
+
+    # if a < left-1:
+    #     print('\n[Recursion 1] {} ~ {}'.format(S[a], S[left-1]))
+    # else:
+    #     print('[Recursion 1] RETURN')
     inplace_quick_sort(S, a, left-1)
+    # if left+1 < b:
+    #     print('\n[Recursion 2] {} ~ {}'.format(S[left+1], S[b]))
+    # else:
+    #     print('[Recursion 2] RETURN')
     inplace_quick_sort(S, left+1, b)
