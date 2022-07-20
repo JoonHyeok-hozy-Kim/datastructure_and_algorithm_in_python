368b3ff3ee8cab957273ddb24792b7e4a3f50c15
HOzy-Joon <danielisdan@naver.com>
2022-07-20 11:25:46 +0900

    LCS implemented

---
+++ TextProcessingAlgorithms/longest_common_sequence.py
@@ -0,0 +1,12 @@
+
+
+def LCS(X, Y):
+    n, m = len(X), len(Y)
+    L = [[0] * (m+1) for k in range(n+1)]
+    for j in range(n):
+        for k in range(m):
+            if X[j] == Y[k]:
+                L[j+1][k+1] = 1 + L[j][k]
+            else:
+                L[j+1][k+1] = max(L[j][k+1], L[j+1][k])
+    return L
