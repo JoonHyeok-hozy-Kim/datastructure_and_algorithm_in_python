483c92c45d7f0ac194466d4847da4cc025b0ab70
HOzy-Joon <danielisdan@naver.com>
2022-07-28 01:33:26 +0900

    DFS_complete implemented

--- DataStructures/graph_dfs.py
+++ DataStructures/graph_dfs.py
@@ -7,6 +7,7 @@ def DFS(g, u, discovered):
     :param u: the starting vertex
     :param discovered: a dictionary mapping each vertex to the edge that was used to discover it during the DFS
     """
+    # print(u)
     for e in g.incident_edges(u):
         v = e.opposite(u)
         if v not in discovered:
@@ -26,3 +27,49 @@ def construct_path(u, v, discovered):
             walk = parent
         path.reverse()
     return path
+
+
+def is_connected(g):
+    arbitrary = next(iter(g.vertices()))    # Get an arbitrary vertex from the graph
+    d = {}
+    DFS(g, arbitrary, d)
+    if len(d) != g.vertex_count():
+        return False
+    elif g._outgoing != g._incoming:
+        incoming_d = {}
+        _incoming_DFS(g, arbitrary, incoming_d)
+        if len(incoming_d) != g.vertex_count():
+            return False
+    return True
+
+
+def _incoming_DFS(g, u, discovered, starting=None):
+    """ Perform DFS using incoming edges. Going reverse way!
+    :param g: the target graph to perform DFS
+    :param u: the starting vertex
+    :param discovered: a dictionary mapping each vertex to the edge that was used to discover it during the DFS
+    """
+    if starting is None:
+        starting = u
+    elif u == starting:     # in order to prevent eternal loop
+        return
+
+    # print(u)
+    for adjacent in g._incoming:
+        for v in g._incoming[adjacent]:
+            if v == u:
+                discovered[adjacent] = g._incoming[adjacent][u]
+                _incoming_DFS(g, adjacent, discovered, starting)
+
+
+def DFS_complete(g):
+    """ Perform DFS for entire graph and return forest as a dictionary.
+    :param g: the target graph
+    :return: Result maps each vertex v to the edge that was used to discover it. (Vertices that are roots of a DFS tree are mapped to None.)
+    """
+    forest = {}
+    for u in g.vertices():
+        if u not in forest:
+            forest[u] = None        # u will be the root of the tree
+            DFS(g, u, forest)
+    return forest
